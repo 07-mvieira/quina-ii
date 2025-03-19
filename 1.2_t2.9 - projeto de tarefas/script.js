5628775c06cca5e8b37b190b198adeb92e93a6ac
07-mvieira <caio.vieira2807@gmail.com>
2025-03-19 11:18:27 -0300

    finalização (?)

--- 1.2_t2.9 - projeto de tarefas/script.js
+++ 1.2_t2.9 - projeto de tarefas/script.js
@@ -1,4 +1,4 @@
-let tarefaId = 1;
+let tarefaId = 0;
 const tarefaCaixa = document.getElementById("tarefa-caixa")
 let tarefaContent = document.getElementById("nova-tarefa").value
 
@@ -20,6 +20,16 @@ function confirmTarefa(){
     novaTarefaP.innerHTML = tarefaContent
     novaTarefa.appendChild(novaTarefaP)
 
+    var novaTarefaButtonDiv = document.createElement("div")
+    novaTarefaButtonDiv.setAttribute("class", "rem-div")
+    novaTarefa.appendChild(novaTarefaButtonDiv)
+
+    var novaTarefaButton = document.createElement("button")
+    novaTarefaButton.innerHTML = `<span id="sign">–</span>`
+    novaTarefaButton.setAttribute("class", "rem")
+    novaTarefaButton.setAttribute("onclick", "removeTarefa()")
+    novaTarefaButtonDiv.appendChild(novaTarefaButton)
+
     document.getElementById("option-caixa").style.display = "block"
     document.getElementById("nova-tarefa-caixa").style.display = "none"
     document.getElementById("nova-tarefa").value = ``
@@ -30,3 +40,13 @@ function cancelTarefa(){
     document.getElementById("nova-tarefa-caixa").style.display = "none"
     document.getElementById("nova-tarefa").value = ``
 }
+
+function removeTarefa(){ //n entendi literalmente nada só copiei do stackoverflow 👍
+    let els = document.getElementsByClassName("rem");
+    for (let i = 0; i < els.length; i++) {
+        els[i].addEventListener('click', function (e) {
+            e.preventDefault();
+            e.target.closest('div.tarefa').remove();
+        });
+    }
+} 
