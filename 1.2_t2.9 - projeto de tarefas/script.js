c85c2821873545f8366afaadbc48d8921c39bfaa
07-mvieira <caio.vieira2807@gmail.com>
2025-03-18 21:36:28 -0300

    eu quase esqueci mds

--- 1.2_t2.9 - projeto de tarefas/script.js
+++ 1.2_t2.9 - projeto de tarefas/script.js
@@ -1,11 +1,32 @@
 let tarefaId = 1;
 const tarefaCaixa = document.getElementById("tarefa-caixa")
+let tarefaContent = document.getElementById("nova-tarefa").value
 
 function addTarefa(){
-    document.getElementById("optionCaixa").style.display = "none"
-    document.getElementById("novaTarefaCaixa").style.display = "block"
+    document.getElementById("option-caixa").style.display = "none"
+    document.getElementById("nova-tarefa-caixa").style.display = "block"
 }
 
 function confirmTarefa(){
-    
+    var novaTarefa = document.createElement("div")
+    tarefaCaixa.appendChild(novaTarefa)
+    novaTarefa.setAttribute("class", "tarefa")
+
+    tarefaId++
+    novaTarefa.setAttribute("id", `tarefa-${tarefaId}`)
+
+    var novaTarefaP = document.createElement("p")
+    tarefaContent = document.getElementById("nova-tarefa").value
+    novaTarefaP.innerHTML = tarefaContent
+    novaTarefa.appendChild(novaTarefaP)
+
+    document.getElementById("option-caixa").style.display = "block"
+    document.getElementById("nova-tarefa-caixa").style.display = "none"
+    document.getElementById("nova-tarefa").value = ``
+}
+
+function cancelTarefa(){
+    document.getElementById("option-caixa").style.display = "block"
+    document.getElementById("nova-tarefa-caixa").style.display = "none"
+    document.getElementById("nova-tarefa").value = ``
 }
